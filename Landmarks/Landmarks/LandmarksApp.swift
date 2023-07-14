cd61ca8bffddb125397fb26edd6a0931de36214b
haopeng <haopeng138@gmail.com>
2023-07-14 21:17:22 +0200

    Chapter1 swiftUI

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 林浩鹏 on 14/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
